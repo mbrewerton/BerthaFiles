c80a29a3706f6f66f28e6e0bf95a4c0cad82fcc4
mbrewerton <m.brewerton94@gmail.com>
2015-05-16 18:44:00 +0100

    Initial Commit

---
+++ BerthaSounds/API/Models/Sound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Sound
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ BerthaSounds/BerthaSounds/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BerthaSounds.Startup))]
+namespace BerthaSounds
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
