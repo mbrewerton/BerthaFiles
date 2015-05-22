376593d0533ca6c9ac4c88e0a38055ed05bba560
mbrewerton <m.brewerton94@gmail.com>
2015-05-22 13:50:52 +0100

    Ninject Install

---
+++ BerthaSounds/API/CustomServiceRegistrationModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Models.DbContexts;
+using Ninject.Modules;
+
+namespace API
+{
+    public class CustomServiceRegistrationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<DbContext>().ToConstructor(x => new BerthaContext());
+        }
+    }
+}
