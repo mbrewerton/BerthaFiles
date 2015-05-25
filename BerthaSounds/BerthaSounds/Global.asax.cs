4611294263158b85fe52c13a3312895812d506b0
Matt <m.brewerton94@gmail.com>
2015-05-25 18:11:07 +0100

    Added js CDN

--- BerthaSounds/BerthaSounds/Global.asax.cs
+++ BerthaSounds/BerthaSounds/Global.asax.cs
@@ -36,23 +36,45 @@ namespace BerthaSounds
         {
             Bundle.Css()
                 .AddDirectory("~/Content/c", true)
+                .AddRemote("~/Content/c", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css")
                 .AsCached("styles", "~/assets/css/styles");
-            
-            Bundle.JavaScript()
-                .Add("~/Content/j/common/jquery-1.10.2.min.js")
-                .Add("~/Content/j/common/angular.min.js")
-                .Add("~/Content/j/common/angular-route.min.js")
-                .Add("~/Content/j/common/angular-resource.min.js")
-                .Add("~/Content/j/common/angular-animate.min.js")
-                .Add("~/Content/j/common/bootstrap.min.js")
-                .Add("~/Content/j/common/jquery.validate.min.js")
-                .AddDirectory("~/Content/j/custom")
-                .AddDirectory("~/Content/j/angular")
-                .AddDirectory("~/Content/j/directives")
-                // Add each area individually
-                .AddDirectory("~/Content/Areas/Admin/Controllers")
-                .AddDirectory("~/Content/Areas/Admin/Resources")
-                .AsCached("jslibs", "~/assets/js/jslibs");
+
+            if (Environment == "Local")
+            {
+                Bundle.JavaScript()
+                    .Add("~/Content/j/common/jquery-1.10.2.min.js")
+                    .Add("~/Content/j/common/angular.min.js")
+                    .Add("~/Content/j/common/angular-route.min.js")
+                    .Add("~/Content/j/common/angular-resource.min.js")
+                    .Add("~/Content/j/common/angular-animate.min.js")
+                    .Add("~/Content/j/common/bootstrap.min.js")
+                    .Add("~/Content/j/common/jquery.validate.min.js")
+                    .AddDirectory("~/Content/j/custom")
+                    .AddDirectory("~/Content/j/angular")
+                    .AddDirectory("~/Content/j/directives")
+                    // Add each area individually
+                    .AddDirectory("~/Content/Areas/Admin/Controllers")
+                    .AddDirectory("~/Content/Areas/Admin/Resources")
+                    .AsCached("jslibs", "~/assets/js/jslibs");
+            }
+            else
+            {
+                Bundle.JavaScript()
+                    .AddRemote("~/Content/j/common", "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js")
+                    .AddRemote("~/Content/j/common", "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min.jss")
+                    .AddRemote("~/Content/j/common", "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-route.min.js")
+                    .AddRemote("~/Content/j/common", "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-resource.min.js")
+                    .AddRemote("~/Content/j/common", "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-animate.min.js")
+                    .AddRemote("~/Content/j/common", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js")
+                    .AddRemote("~/Content/j/common", "http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js")
+                    .AddDirectory("~/Content/j/custom")
+                    .AddDirectory("~/Content/j/angular")
+                    .AddDirectory("~/Content/j/directives")
+                    // Add each area individually
+                    .AddDirectory("~/Content/Areas/Admin/Controllers")
+                    .AddDirectory("~/Content/Areas/Admin/Resources")
+                    .AsCached("jslibs", "~/assets/js/jslibs");
+            }
         }
     }
 }
